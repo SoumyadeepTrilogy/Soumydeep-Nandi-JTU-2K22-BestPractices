49d08a9499c4e2b5b1d26f66de85c8bb101cc7df
SoumyadeepTrilogy <soumyadeep.nandi@trilogy.com>
2022-05-21 19:12:19 +0000

    Logger statements added

--- restapi/views.py
+++ restapi/views.py
@@ -45,6 +45,7 @@ def logout(request) -> Response:
         Deletes Authetication Token 
     '''
     request.user.auth_token.delete()
+    logger.info("User Authentication Token deleted successfully")
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -53,6 +54,7 @@ def balance(request) -> Response:
     '''
         Used to get final balances
     '''
+    logger.info("Final balance request initiated")
     user = request.user
     expenses = Expenses.objects.filter(users__in=user.expenses.all())
     final_balance = {}
@@ -68,6 +70,7 @@ def balance(request) -> Response:
     final_balance = {k: v for k, v in final_balance.items() if v != 0}
 
     response = [{"user": k, "amount": int(v)} for k, v in final_balance.items()]
+    logger.info("Final balance calculated and executed successfully")
     return Response(response, status=200)
 
 
@@ -75,6 +78,7 @@ def normalize(expense):
     '''
         Normalizes Expenses
     '''
+    logger.info("Normalization of expense initiated")
     user_balances = expense.users.all()
     dues = {}
     for user_balance in user_balances:
@@ -94,12 +98,11 @@ def normalize(expense):
             start += 1
         else:
             end -= 1
+    logger.info("Normalization of expense executed")
     return balances
 
 
 class user_view_set(ModelViewSet):
-    '''
-        
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
@@ -116,23 +119,37 @@ class group_view_set(ModelViewSet):
     serializer_class = GroupSerializer
 
     def get_queryset(self):
+        '''
+            Gets Queryset of Users
+        '''
+        logger,info("get_queryset function initiated")
         user = self.request.user
         groups = user.members.all()
         if self.request.query_params.get('q', None) is not None:
             groups = groups.filter(name__icontains=self.request.query_params.get('q', None))
+        logger,info("get_queryset function executed")
         return groups
 
     def create(self, request, *args, **kwargs)b -> Response:
+        '''
+            Adds user to a group
+        '''
+        logger.info("Creation of group initiated")
         user = self.request.user
         data = self.request.data
         group = Groups(**data)
         group.save()
         group.members.add(user)
         serializer = self.get_serializer(group)
+        logger.info("Creation of group and addition of users to it executed")
         return Response(serializer.data, status=201)
 
     @action(methods=['put'], detail=True)
     def members(self, request, pk=None) -> Response:
+        '''
+            Adding members to a group
+        '''
+        logger.info("members function initiated")
         group = Groups.objects.get(id=pk)
         if group not in self.get_queryset():
             raise UnauthorizedUserException()
@@ -146,19 +163,29 @@ class group_view_set(ModelViewSet):
             for user_id in removed_ids:
                 group.members.remove(user_id)
         group.save()
+        logger.info("logger function executed")
         return Response(status=204)
 
     @action(methods=['get'], detail=True)
     def expenses(self, _request, pk=None) -> Response:
+        '''
+            Group Expenses Returned
+        '''
+        logger.info("expenses function initiated")
         group = Groups.objects.get(id=pk)
         if group not in self.get_queryset():
             raise UnauthorizedUserException()
         expenses = group.expenses_set
         serializer = ExpensesSerializer(expenses, many=True)
+        logger.info("expenses function executed")
         return Response(serializer.data, status=200)
 
     @action(methods=['get'], detail=True)
     def balances(self, _request, pk=None) -> Response:
+        '''
+            Returns balance
+        '''
+        logger.info("balance function initiated")
         group = Groups.objects.get(id=pk)
         if group not in self.get_queryset():
             raise UnauthorizedUserException()
@@ -184,7 +211,7 @@ class group_view_set(ModelViewSet):
                 start += 1
             else:
                 end -= 1
-
+        logger.info("balance function executed")
         return Response(balances, status=200)
 
 
@@ -205,6 +232,10 @@ class expenses_view_set(ModelViewSet):
 @authentication_classes([])
 @permission_classes([])
 def logProcessor(request) -> Response:
+    '''
+        Log_files processed
+    '''
+    logger.info("logprocessor initiated")
     data = request.data
     num_threads = data['parallelFileProcessingCount']
     log_files = data['logFiles']
@@ -219,17 +250,27 @@ def logProcessor(request) -> Response:
     cleaned = transform(sorted_logs)
     data = aggregate(cleaned)
     response = response_format(data)
+    logger.info("logprocessor executed")
     return Response({"response":response}, status=status.HTTP_200_OK)
 
 def sort_by_time_stamp(logs):
+    '''
+        Sorts log data by time
+    '''
+    logger.info("Sorting data by time initiated")
     data = []
     for log in logs:
         data.append(log.split(" "))
     # print(data)
     data = sorted(data, key=lambda elem: elem[1])
+    logger.info("Sorting data by time executed")
     return data
 
 def response_format(raw_data):
+    '''
+        Edits response format of raw_data
+    '''
+    logger.info("Editing response format of raw_data initiated")
     response = []
     for timestamp, data in raw_data.items():
         entry = {'timestamp': timestamp}
@@ -239,19 +280,29 @@ def response_format(raw_data):
             logs.append({'exception': exception, 'count': count})
         entry['logs'] = logs
         response.append(entry)
+    logger.info("Editing response format of raw_data executed")
     return response
 
 def aggregate(cleaned_logs):
+    '''
+        Aggregates clean logs
+    '''
+    logger.info("Aggregation of clean logs initiated")
     data = {}
     for log in cleaned_logs:
         [key, text] = log
         value = data.get(key, {})
         value[text] = value.get(text, 0)+1
         data[key] = value
+    logger.info("Aggregation of clean logs executed")
     return data
 
 
 def transform(logs):
+    '''
+        Transforms logs
+    '''
+    logger.info("Trasformation of log initiated")
     result = []
     for log in logs:
         [_, timestamp, text] = log
@@ -274,7 +325,7 @@ def transform(logs):
 
         result.append([key, text])
         print(key)
-
+    logger.info("Trasformation of log executed")
     return result
 
 
@@ -287,10 +338,12 @@ def multiThreadedReader(urls, num_threads):
     """
         Read multiple files through HTTP
     """
+    logger.info("multithreadreader function initiated")
     result = []
     for url in urls:
         data = reader(url, 60)
         data = data.decode('utf-8')
         result.extend(data.split("\n"))
     result = sorted(result, key=lambda elem:elem[1])
+    logger.info("multithreadreader function executed")
     return result
