e30979db71b22022b21cdcfd755f1116b0786a1b
SoumyadeepTrilogy <soumyadeep.nandi@trilogy.com>
2022-05-21 18:37:08 +0000

    Added logger statements

--- restapi/views.py
+++ restapi/views.py
@@ -4,6 +4,18 @@ from decimal import Decimal
 import urllib.request
 from datetime import datetime
 
+import logging
+logger=logging.getlogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter=logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+file_handler=logging.FileHandler('views.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
