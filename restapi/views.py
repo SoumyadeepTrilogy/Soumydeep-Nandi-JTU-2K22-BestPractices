f25423eb73e5f3e449d37a55ae75e639822e76cc
SoumyadeepTrilogy <105647383+SoumyadeepTrilogy@users.noreply.github.com>
2022-05-26 14:28:41 +0530

    Modified logger statement

--- restapi/views.py
+++ restapi/views.py
@@ -5,16 +5,8 @@ import urllib.request
 from datetime import datetime
 
 import logging
-logger=logging.getlogger(__name__)
-logger.setLevel(logging.DEBUG)
 
-formatter=logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-
-file_handler=logging.FileHandler('views.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
+logger = logging.getLogger(__name__)
 
 from django.http import HttpResponse
 from django.contrib.auth.models import User
